e4ec038dec9ab20dd339de86965ebfdc51da64d8
unknown <zmike808@gmail.com>
2014-11-11 13:21:25 -0500

    ups, this is what it should be, bvt forgot i commited this to clusterfuck branch instead

--- testing/testing.py
+++ testing/testing.py
@@ -1,11 +1,11 @@
-from geopy.distance import distance
+from geopy.distance import distance, great_circle
 import multiprocessing
 import json
 import itertools
 import math
 import time
 import sys
-search_radius = 17000.0
+import gc
  
 class gThread(multiprocessing.Process):
   def __init__(self, list, tid, queue):
@@ -38,14 +38,17 @@ class Geodata:
   def __init__(self, lat, lng, id=-1, pid=-1):
     self.lat = lat
     self.lng = lng
-    self.clat = self.lat * (math.pi * 6378137 / 180)
-    self.clng = self.lng * (math.pi * 6378137 / 180)
+    #self.clat = self.lat * (math.pi * 6378137 / 180)
+    #self.clng = self.lng * (math.pi * 6378137 / 180)
     self.id = id
     self.place_id = pid
-    self.origin_dist = distance(ocords, (self.lat, self.lng)).meters
+    self.origin_dist = great_circle(ocords, (self.lat, self.lng)).meters
+  
+  def __iter__(self):
+    return iter((self.lat, self.lng))
   
   def __str__(self):
-    s = "(lat,lng): (%s, %s)\norigin_dist: %s id: %s pid: %s" % (str(self.lat), str(self.lng), self.origin_dist, self.id, self.place_id)
+    s = "lat:\n%s\nlng:\n%s\norigin_dist:\n%s\nid: %s pid: %s" % (str(self.lat), str(self.lng), self.origin_dist, self.id, self.place_id)
     return s
     
 def build_geodata(json):
@@ -53,42 +56,45 @@ def build_geodata(json):
   for j in json:
     g = Geodata(j["geometry"]["location"]["lat"], j["geometry"]["location"]["lng"], j["id"], j["place_id"])
     geodatas.append(g)
-  return sorted(geodatas, key=lambda x: x.origin_dist)
+  geodatas.sort(key=lambda x: x.origin_dist)
+  return geodatas
 
 def compute_displacement(list):
-  d = 0.0
-  points = list
-  points.append(points[0])
-  plen = len(points) - 1
-  for x in range(plen):
-    p2 = (points[x + 1].lat, points[x+1].lng)
-    p1 = (points[x].lat, points[x].lng)
-    dtmp = distance(p1, p2).meters
-    d = d + dtmp
-  return d
+  list.append(list[0])
+  return great_circle(*list).meters
 
-def main():
+def main(start_time):
+  #gc.set_debug(gc.DEBUG_UNCOLLECTABLE)
+  #gc.set_debug(gc.DEBUG_STATS)
   f1 = open("haircut.json")
   f2 = open("grocery.json")  
   f3 = open("car_wash.json")
-  
-  start_time = time.time()
-  
-  output = open("output.txt",'w')
-  
+      
   hjson = (json.load(f1))["results"]
   gsjson = (json.load(f2))["results"]
   gamestopjson = (json.load(f3))["results"]
+  f1.close()
+  f2.close()
+  f3.close()
+  f1 = f2 = f3 = None
 
   haircut_geodata = build_geodata(hjson)
   grocery_geodata = build_geodata(gsjson)
   gamestop_geodata = build_geodata(gamestopjson)
+  hjson = gsjson = gamestopjson = None
 
   origin_coords = Geodata(42.730678, -73.686662)
+  
   tlist = [haircut_geodata, grocery_geodata, gamestop_geodata]#haircut_geodata + grocery_geodata + gamestop_geodata
   product = list(itertools.product(*tlist))
+  haircut_geodata = grocery_geodata = gamestop_geodata = tlist = None
+  #print("time before forceing gc collection: ", str(time.time() - start_time))
+  gc.collect()
+  #print("time after force gc", str(time.time() - start_time))
   
   geoareas = []
+  threads = [] 
+
   mpq = multiprocessing.Queue()
 
   if(len(sys.argv) == 2):
@@ -97,35 +103,42 @@ def main():
     nprocs = 10
 
   chunksize = int(math.ceil(len(product) / float(nprocs)))
-  threads = []
-  
+
   for i in range(nprocs):#for i in range(chunks):
     c = product[chunksize * i:chunksize * (i + 1)]
     t = gThread(c, i, mpq)
     t.start()
     threads.append(t)
   
+  product = None
+  #print("time before forceing gc collection: ", str(time.time() - start_time))
+  gc.collect()
+  #print("time after force gc", str(time.time() - start_time))
+
   for i in range(nprocs):
     geoareas.extend(mpq.get())
   
   for t in threads:
     t.join()
   
+  mpq = threads = None 
+  #print("time before forceing gc collection: ", str(time.time() - start_time))
+  gc.collect()
+  #print("time after force gc", str(time.time() - start_time))
+  
   print("PRE-SORT TIME DELTA:", str(time.time() - start_time))
-  sortedareas = sorted(geoareas, key=lambda x: x.delta)
+  geoareas.sort(key=lambda x: x.delta)
   print("POST-SORT TIME DELTA:", str(time.time() - start_time))
+  
+  output = open("output.txt",'w')
 
   for x in range(5):
-    print(sortedareas[x], file=output)
+    print(geoareas[x], file=output)
   
   output.close()
-  
-  end_time = time.time()
-  delta = end_time - start_time
-  print("process took a total of %s time!" % (str(delta)))
-  f1.close()
-  f2.close()
-  f3.close()
+  output = None
 
 if __name__ == '__main__':
-  main()
+  start_time = time.time()
+  main(start_time)
+  print("process took a total of %s time!" % (str(time.time() - start_time)))
